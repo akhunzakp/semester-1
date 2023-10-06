269e56215fd2ee01f44cff3b0bec079b6bdeeff4
akhunzakp <yuma.akhunza@gmail.com>
2023-10-06 14:50:08 +0700

    commit

--- jobsheet6/Selection2Exp226.java
+++ jobsheet6/Selection2Exp226.java
@@ -15,14 +15,24 @@ public class Selection2Exp226{
         angle3 = input26.nextInt();
         totalAngle = angle1+angle2+angle3;
     
-        if(totalAngle == 180)
+        //if(totalAngle == 180)
+            //if(angle1==90 || angle2==90 || angle3==90)
+                //System.out.println("Right triangle");
+
+            //else
+                //System.out.println("Not a right triangle");
+
+        //else
+            //System.out.println("Not a triangle");
+        
             if(angle1==90 || angle2==90 || angle3==90)
-                System.out.println("Right triangle");
-
-            else
-                System.out.println("Not a right triangle");
-        else
-            System.out.println("Not a triangle");
-
+            System.out.println("Right triangle");
+            else if (angle1 == angle2 && angle2 == angle3){
+                System.out.println("Right This is an equilateral triangle");
+                }else if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3){
+                System.out.println("This is an isosceles triangle");
+                }else{
+                System.out.println("not a triangle");
+                }
     }
 }
